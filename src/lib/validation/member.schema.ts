0a22c96f000689540e5facf095698a0b90f61a1b
Ali Musthafa Kamal <alimusthafakamal@gmail.com>
2025-07-14 03:20:27 +0700

    add member validation

---
+++ src/lib/validation/member.schema.ts
@@ -0,0 +1,31 @@
+// src/lib/validation/member.schema.ts
+import * as z from "zod";
+
+export const memberFormSchema = z.object({
+  // Field Wajib
+  name: z.string().min(3, "Nama lengkap minimal 3 karakter."),
+  nomorAnggota: z
+    .string()
+    .regex(
+      /^\d{3}\/KPA EMC²\/\d{4}$/,
+      "Format harus 3 digit/KPA EMC²/4 digit (contoh: 174/KPA EMC²/2022)"
+    ),
+  status: z.enum(["Aktif", "Alumni", "Non-aktif"], {
+    required_error: "Anda perlu memilih status keanggotaan.",
+  }),
+
+  // Field Opsional
+  jurusan: z.string().optional(),
+  nomorTelepon: z
+    .string()
+    .min(10, "Nomor telepon minimal 10 digit.")
+    .optional()
+    .or(z.literal("")),
+  avatarUrl: z
+    .string()
+    .url("URL avatar tidak valid.")
+    .optional()
+    .or(z.literal("")),
+});
+
+export type MemberFormValues = z.infer<typeof memberFormSchema>;
