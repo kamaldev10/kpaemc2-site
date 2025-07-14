08abe853105a37ea621cfd3afe1ab1d30bf58d4e
Ali Musthafa Kamal <alimusthafakamal@gmail.com>
2025-07-14 19:03:15 +0700

    add latest migration table

---
+++ prisma/migrations/20250714010856_updated_about_info_table/migration.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE "AboutInfo" ADD COLUMN     "bornDate" TEXT NOT NULL DEFAULT '10 Oktober 1984',
+ADD COLUMN     "motto" TEXT NOT NULL DEFAULT 'Bergerak Satu Asa, Berbekal Alam Lestari!';
