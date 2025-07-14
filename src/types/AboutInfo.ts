30757ca82f30fdd8934b334b5cd23e80b87b6ed9
Ali Musthafa Kamal <alimusthafakamal@gmail.com>
2025-07-14 19:06:00 +0700

    add ABout Info Types

--- src/types/AboutInfo.ts
+++ src/types/AboutInfo.ts
@@ -1,15 +1,21 @@
-export type StructurePeriod = {
+import { type JsonValue } from "@prisma/client/runtime/library";
+
+// Tipe untuk objek di dalam array 'structure'
+export type StructureInfo = {
   period: string;
-  image: string; // Path ke gambar/foto struktur
-  chairmanName: string; // Menyimpan nama ketua untuk referensi
+  image: string;
+  chairmanName: string;
 };
 
-// Tipe data utama untuk seluruh data "Tentang Kami"
+// Tipe utama yang cocok dengan skema Prisma
 export type About = {
+  id: number;
   orgName: string;
+  bornDate: string;
+  motto: string;
   description: string;
   vision: string;
   mission: string[];
   activePeriod: string;
-  structure: StructurePeriod[];
+  structure: JsonValue; // Tipe dari Prisma untuk JSON
 };
