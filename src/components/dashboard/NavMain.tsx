72c5462803e7a240a5bf910b12c0364e0072d512
Ali Musthafa Kamal <alimusthafakamal@gmail.com>
2025-07-17 17:04:12 +0700

    Update Navigation(Main)

--- src/components/dashboard/NavMain.tsx
+++ src/components/dashboard/NavMain.tsx
@@ -3,7 +3,7 @@
 import * as React from "react";
 import Link from "next/link";
 import { ChevronRight, type LucideIcon } from "lucide-react";
-import { cn } from "@/lib/utils"; // <-- Impor 'cn' untuk kelas kondisional
+import { cn } from "@/lib/utils/utils"; // <-- Impor 'cn' untuk kelas kondisional
 
 import {
   Collapsible,
