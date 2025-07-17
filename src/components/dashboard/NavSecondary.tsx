af6ee7a4999f96cfe84b1a94187030a8915bb994
Ali Musthafa Kamal <alimusthafakamal@gmail.com>
2025-07-17 17:01:36 +0700

    update Navigation

--- src/components/dashboard/NavSecondary.tsx
+++ src/components/dashboard/NavSecondary.tsx
@@ -3,7 +3,7 @@
 import * as React from "react";
 import Link from "next/link";
 import { type LucideIcon } from "lucide-react";
-import { cn } from "@/lib/utils"; // <-- Impor 'cn'
+import { cn } from "@/lib/utils/utils"; // <-- Impor 'cn'
 
 import {
   SidebarGroup,
