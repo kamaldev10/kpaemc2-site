9e0af2b30deb28245be6f698f62211ebc9001a0f
Ali Musthafa Kamal <alimusthafakamal@gmail.com>
2025-07-18 07:37:30 +0700

    chore: update import type member

--- src/components/dashboard/members/ExportMembersButton.tsx
+++ src/components/dashboard/members/ExportMembersButton.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { type Member } from "@/lib/data/MembersData";
+import { type Member } from "@/types/Member";
 import { Download } from "lucide-react";
 import * as XLSX from "xlsx";
 
