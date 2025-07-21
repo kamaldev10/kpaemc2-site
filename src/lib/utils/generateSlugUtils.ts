2b9d639615502ed1b6581c613e8f1808fea3b857
Ali Musthafa Kamal <alimusthafakamal@gmail.com>
2025-07-21 07:55:49 +0700

    refactor: delete nullToUndefined.ts, generateSlugUtils.ts

--- src/lib/utils/generateSlugUtils.ts
+++
@@ -1,8 +0,0 @@
-export function generateSlug(title: string) {
-  return title
-    .toLowerCase()
-    .replace(/[^a-z0-9\s-]/g, "") // Hapus karakter non-alfanumerik kecuali spasi dan strip
-    .trim()
-    .replace(/\s+/g, "-") // Ganti spasi dengan strip
-    .replace(/-+/g, "-"); // Ganti beberapa strip menjadi satu
-}
