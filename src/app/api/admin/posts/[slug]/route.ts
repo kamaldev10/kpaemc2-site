db874e151201f15db60f5f0e13bd5f144e05de9b
Ali Musthafa Kamal <alimusthafakamal@gmail.com>
2025-07-21 08:03:53 +0700

    refactor: applicate custome error handling

--- src/app/api/admin/posts/[slug]/route.ts
+++ src/app/api/admin/posts/[slug]/route.ts
@@ -1,33 +1,28 @@
-// src/app/api/admin/posts/[slug]/route.ts
 import { NextResponse } from "next/server";
 import { PostService } from "@/services/post.service";
-import { z } from "zod";
+import { errorHandler } from "@/lib/utils/errors";
 
 type RouteParams = { params: { slug: string } };
 
 // PUT: Mengupdate postingan berdasarkan slug
 export async function PUT(request: Request, { params }: RouteParams) {
   try {
-    const data = await request.json();
-    const updatedPost = await PostService.update(params.slug, data);
+    const { slug } = params;
+    const formData = await request.formData();
+    const updatedPost = await PostService.update(slug, formData);
     return NextResponse.json(updatedPost);
   } catch (error) {
-    if (error instanceof z.ZodError) {
-      return new NextResponse(JSON.stringify(error.issues), { status: 400 });
-    }
-    return new NextResponse("Internal Server Error", { status: 500 });
+    return errorHandler(error);
   }
 }
 
 // DELETE: Menghapus postingan berdasarkan slug
 export async function DELETE(request: Request, { params }: RouteParams) {
   try {
-    await PostService.delete(params.slug);
-    return new NextResponse(null, { status: 204 }); // No Content
+    const { slug } = params;
+    await PostService.delete(slug);
+    return new NextResponse(null, { status: 204 });
   } catch (error) {
-    if (error instanceof z.ZodError) {
-      return new NextResponse(JSON.stringify(error.issues), { status: 400 });
-    }
-    return new NextResponse("Internal Server Error", { status: 500 });
+    return errorHandler(error);
   }
 }
