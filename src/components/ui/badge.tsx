53edc9afd480006f715b2c136dac135cc650c69b
Ali Musthafa Kamal <alimusthafakamal@gmail.com>
2025-07-22 17:19:28 +0700

    style: add variants

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -17,6 +17,10 @@ const badgeVariants = cva(
           "border-transparent bg-destructive text-white [a&]:hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
         outline:
           "text-foreground [a&]:hover:bg-accent [a&]:hover:text-accent-foreground",
+        release:
+          "border-transparent bg-blue-500 text-blue-50 hover:bg-blue-500/80",
+        collaboration:
+          "border-transparent bg-teal-500 text-teal-50 hover:bg-teal-500/80",
       },
     },
     defaultVariants: {
